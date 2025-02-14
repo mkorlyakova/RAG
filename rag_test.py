0e9dc83447d7d453c96c9aa65fe165d1019d5150
mkorlyakova <mkorlyakova@yandex.com>
2025-02-14 22:12:26 +0000

    test

--- rag_test.py
+++ rag_test.py
@@ -2,6 +2,7 @@ import os
 import glob
 import numpy as np
 import pandas as pd
+import time
 
 # from langchain_community.tools.tavily_search import TavilySearchResults
 # from langchain_community.tools import DuckDuckGoSearchRun, DuckDuckGoSearchResults
@@ -12,6 +13,7 @@ import pandas as pd
 
 import torch.nn.functional as F
 from torch import Tensor
+import torch
 
 print('TORCH GPU:', torch.cuda.is_available())
 
@@ -23,6 +25,51 @@ from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 from langchain.schema import Document
 from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.prompts import PromptTemplate
+
+#parser = JsonOutputParser(pydantic_object=grade)
+
+prompt = PromptTemplate(
+    template=""" Ты эксперт косметолок \n\n.
+    Отвечай только по русски.\n\n 
+    Избегай повторов. \n\n 
+    Отвечай только на вопросы темы косметология. \n\n
+    Отвечай на вопросы с помощью полученного контекста:  \n\n {context} \n\n.\n\n 
+    Добавь к ответу 3-5 предложений по теме вопроса: {question}.\n\n
+    Верни результат в форме JSON, где в поле \'recomended product\' укажи имя имя продукта из контекста, а в поле \'answer\' запиши ответ на вопрос \n\n
+    Объясни свои рекомндации: 
+    """,
+    input_variables=["question","context"])
+
+prompt_list = PromptTemplate(
+        template="""Ты эксперт косметолог. \n
+        Отвечай только по русски.\n
+        Избегай повторов. \n
+        Отвечай только на вопросы темы косметология. \n
+        Если вопрос не по теме косметология, тогда сообщи, что не можешь ответить
+        Используй для рекомендаций только продукты из  контекста:  \n{context} \n
+        Добавь к ответу 3-5 предложений по теме вопроса. \n
+        Верни результат без какой-либо преамбулы в форме JSON, где в поле \'recomended product\' укажи имена 2-3 продуктов из контекста подходящие {question}, а в поле \'ans>
+ 
+        Это наш вопрос:
+        \n ------- \n
+        {question}.
+        \n ------- \n
+         : 
+        """,
+        input_variables=["question"],
+    )
+
+llm = ChatOllama(model='deepseek-r1:32b', temperature=0)
+chain = prompt | llm | StrOutputParser() #parser
+
+query = 'Опиши утренний уход за лицом.'
+context = ''
+better_question = chain.invoke({"question": query, 'context': context})
+print(better_question)
 
 def average_pool(last_hidden_states: Tensor,
                  attention_mask: Tensor) -> Tensor:
@@ -84,9 +131,9 @@ def vector_search(user_query,dataset_df):
     print(ind[:5])
 
     # Execute the search
-    results = dataset_df.iloc[ind[-4:],:].loc[:, ["fullplot", "title", "genres"]].to_dict('index')#to_json(orient='records', lines=True).splitlines()
+    results = dataset_df.iloc[ind[-4:],:].loc[:, ["text"]].values.tolist()
     print(results)
-    return results
+    return results, ind[-4:]
 
 def grade_documents(state):
     """
@@ -167,10 +214,7 @@ def transform_query(state, local_llm = llm):
     )
 
     # Grader
-    # LLM
     llm = local_llm
-    
-
     # Prompt
     chain = prompt | llm | StrOutputParser()
     better_question = chain.invoke({"question": question, 'context':context})
@@ -215,21 +259,73 @@ embeddings = F.normalize(embeddings, p=2, dim=1)
 scores = (embeddings[:1] @ embeddings[1:].T) * 100
 print(scores.tolist())
 
-dataset_df = pd.read_csv('/home/mariya/PycharmProjects/RAG/data_goldenaple_prepeared_to_embeding.csv', index_col=0)
-
-dataset_df["embedding"] = dataset_df["text"].apply(get_embedding)
-
+dataset_df = pd.read_csv('data_goldenaple_prepeared_to_embeding.csv', index_col=0)
 print(dataset_df.head())
 
+dataset_df = dataset_df.iloc[:1000, :]
 
-
-dataset_df["embedding"]
+dataset_df["embedding"] = dataset_df["text"].apply(get_embedding)
+print(dataset_df.shape)
 
 
 documents = []
 
 
-web_results = web_search(question='')
+documents, ind = vector_search(query, dataset_df)
+
+web_results = web_search(question=query)
 documents.append(web_results)
 
+print(len(documents))
 
+df_list = pd.read_json('titles.json')
+documents_list = df_list.iloc[:,0].tolist()
+
+n3 = 3
+for model_name in  ['deepseek-r1:32b', 'qwen:32b', 'llama3.3:latest', 'phi4:latest', 'mistral:latest', 'gemma2:27b', 'llama3.2-vision:latest','deepseek-r1:70b', 'deepseek-r1:latest' ]:
+    count=0
+    print(model_name)
+    with open(model_name + '_rez.txt','a+') as f:
+        f.write('id,query,  no_context,t0,  context, t1, list_context, t2, docs, web \n')
+    for  query in ['Опиши утренний уход за лицом.', 'Опиши методы RAG для анализа документов', "Дай рекомендации по лечению акне",]:
+        print(query)
+        try:
+            llm = ChatOllama(model=model_name, temperature=0.1)
+            chain = prompt | llm | StrOutputParser() #parser
+            t0 = time.time()
+            better_question_no_context = []
+            for j in range(n3):
+                context = ''
+                better_question_no_context.append( chain.invoke({"question": query, 'context': context}))
+            t0 = time.time() -  t0
+            t0 = t0/n3
+            #print(better_question_no_context)
+
+            llm = ChatOllama(model=model_name, temperature=0.1)
+            chain = prompt | llm | StrOutputParser() 
+
+            t1 = time.time()
+
+            context = ' ; '.join(documents[0])
+            better_question = []
+            for j in range(n3):
+                better_question.append( chain.invoke({"question": query, 'context': context}))
+            t1 = (time.time() -  t1)/n3
+            #print(better_question)
+
+            llm = ChatOllama(model=model_name, temperature=0.1)
+            chain = prompt_list | llm | StrOutputParser() 
+            better_question_list = []
+            t2 = time.time()
+            for j in range(n3):
+                context = ' ; '.join(documents_list)
+                better_question_list.append(chain.invoke({"question": query, 'context': context}))
+            t2 = (time.time() -  t2)/n3
+            #print(better_question_list)
+            with open(model_name + '_rez.txt', 'a+') as f:
+                for j in range(n3):
+                    s = ', '.join([str(count), query, better_question_no_context[j], str(t0), better_question[j], str(t1), better_question_list[j], str(t2), '['+','.join([str(i) for i in ind]) + ']', web_results.page_content]) 
+                    f.write(s + ' \n')
+                    count += 1
+        except:
+            print(model_name + ': error')
